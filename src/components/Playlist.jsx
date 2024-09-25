a0cedb29f1a7576af262b908a300fee05a0be599
Anver Ismagilov <anver8932@gmail.com>
2024-09-25 18:33:09 -0400

    Fix songs order in Playlist.jsx

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -7,15 +7,15 @@ import song3 from "../assets/songs/Куок - МОИ РОБОТЫ.mp3";
 import song4 from "../assets/songs/Три дня дождя - Где ты.mp3";
 import cover1 from "../assets/covers/lida-ЧСВ.jpeg";
 import cover2 from "../assets/covers/Linkin Park - What I've Done.jpeg";
-import cover3 from "../assets/covers/tdd-gde-ti.jpeg";
-import cover4 from "../assets/covers/Куок - Мои роботы.jpeg";
+import cover4 from "../assets/covers/tdd-gde-ti.jpeg";
+import cover3 from "../assets/covers/Куок - Мои роботы.jpeg";
 
 // List of songs with their details
 const songs = [
   { id: 1, title: "lida - ЧСВ", cover: cover1, file: song1 },
   { id: 2, title: "Linkin Park - What I've Done", cover: cover2, file: song2 },
-  { id: 3, title: "Три дня дождя - Где ты", cover: cover3, file: song3 },
-  { id: 4, title: "Куок - Мои роботы", cover: cover4, file: song4 },
+  { id: 3, title: "Куок - Мои роботы", cover: cover3, file: song3 },
+  { id: 4, title: "Три дня дождя - Где ты", cover: cover4, file: song4 },
 ];
 
 const Playlist = ({ setCurrentSong }) => {
